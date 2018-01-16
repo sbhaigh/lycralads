f71c91fc3324e535dea592f3477f9fcd51191f29
Steve Haigh <steve@stevehaigh.net>
2018-01-16 13:08:37 +0000

    Add basic page loader construct while processign google sheets data

--- js/llstats.js
+++ js/llstats.js
@@ -47,6 +47,8 @@ function showInfo(data, tabletop) {
         html = html + "<th>" + thisYearsSheet.columnNames[i] + "</th>";
     }
     console.log(summaryStats);
+        // data loaded, hide the loader
+        document.getElementById('loader_wrapper').style.display = 'none';
     html = html + "</tr></thead><tbody class='table-striped'>";
 
     for(i = 0; i < thisYearsSheet.elements.length; i++) {
